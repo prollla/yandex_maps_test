50a626ead1987b80e99ddc5cc735d22a5e812a0c
prollla <95498027+prollla@users.noreply.github.com>
2024-10-26 15:25:46 +0300

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,9 @@
 import pytest
+import sys
+import os
+from endpoints.yandex_endpoint import YandexEndpoint
 
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 @pytest.fixture()
 def yandex_fixture():
